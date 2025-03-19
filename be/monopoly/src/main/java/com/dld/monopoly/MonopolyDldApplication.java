e0f0add7dfde10c11ffc417143bbe8c3c7adff76
pleewson <adrianplewait@gmail.com>
2025-03-19 21:51:22 +0100

    first commit +be

---
+++ be/monopoly/src/main/java/com/dld/monopoly/MonopolyDldApplication.java
@@ -0,0 +1,13 @@
+package com.dld.monopoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolyDldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopolyDldApplication.class, args);
+	}
+
+}
---
+++ be/monopoly/src/test/java/com/dld/monopoly/MonopolyDldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dld.monopoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolyDldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
