76ff54f5345d134d065c549de4b1bf9d32a66276
pleewson <adrianplewait@gmail.com>
2025-03-26 16:54:55 +0100

    +findFieldBy id/name +tests

--- be/monopoly/src/test/java/com/dld/monopoly/service/GameServiceTest.java
+++ be/monopoly/src/test/java/com/dld/monopoly/service/GameServiceTest.java
@@ -1,16 +1,25 @@
 package com.dld.monopoly.service;
 
+import com.dld.monopoly.model.Game;
 import com.dld.monopoly.model.GameManager;
+import com.dld.monopoly.model.fields.Field;
+import com.dld.monopoly.model.fields.FieldType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameServiceTest {
-    @Mock
-    private GameManager gameManager;
-    private GameService gameService = new GameService(gameManager);
 
+    private static Game game;
+    private static GameManager gameManager = new GameManager();
+    private static GameManagerService gameManagerService = new GameManagerService(gameManager);
+    private GameService gameService = new GameService(gameManager, gameManagerService);
+
+    @BeforeAll
+    static void initializeGame() {
+        game = gameManagerService.createNewGame();
+    }
 
     @Test
     void throwDices_shouldReturnDicesBetween1and6() {
@@ -21,5 +30,35 @@ class GameServiceTest {
 
         assertTrue(dices[1] > 0);
         assertTrue(dices[1] < 7);
+    }
+
+
+    @Test
+    void findFieldById_whenSearchingPropertyFields_shouldReturnCorrect() {
+        Field field = gameService.findFieldById(game, 11); //id:11 - jail
+        assertEquals(field.getId(), 11);
+        assertEquals(field.getFieldType(), FieldType.JAIL);
+        assertEquals(field.getName(), "JAIL");
+
+
+        field = gameService.findFieldById(game, 40); //id:40 - last field
+        assertEquals(field.getId(), 40);
+        assertEquals(field.getFieldType(), FieldType.PROPERTY);
+        assertEquals(field.getName(), "BOARDWALK");
+    }
+
+
+    @Test
+    void findFieldByName_whenSearchingPropertyFields_shouldReturnCorrect() {
+        Field field = gameService.findFieldByName(game, "JAIL"); //id:11 - jail
+        assertEquals(field.getId(), 11);
+        assertEquals(field.getFieldType(), FieldType.JAIL);
+        assertEquals(field.getName(), "JAIL");
+
+
+        field = gameService.findFieldByName(game, "BOARDWALK"); //id:40 - last field
+        assertEquals(field.getId(), 40);
+        assertEquals(field.getFieldType(), FieldType.PROPERTY);
+        assertEquals(field.getName(), "BOARDWALK");
     }
 }
