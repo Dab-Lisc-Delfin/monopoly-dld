5f4d61a97745eff110e158c5e654501085ffc2b4
pleewson <adrianplewait@gmail.com>
2025-07-02 23:19:29 +0200

    +doc, Properties: Player owner -> int ownerId

--- be/monopoly/src/main/java/com/dld/monopoly/model/fields/RailroadProperty.java
+++ be/monopoly/src/main/java/com/dld/monopoly/model/fields/RailroadProperty.java
@@ -9,7 +9,7 @@ public class RailroadProperty extends RentableProperty {
     public RailroadProperty(int id, String name) {
         super(id, name, FieldType.RAILROADS, 200);
         this.isAvailable = true;
-        this.owner = null;
+        this.ownerId = 0;
         this.oneRailroadRent = 25;
         this.twoRailroadRent = 50;
         this.threeRailroadRent = 100;
@@ -22,28 +22,28 @@ public class RailroadProperty extends RentableProperty {
     private final int fourRailroadRent;
     private boolean isAvailable;
 
-    public int getRentCost() {
-        int ownedRailroads = getAmountOwnedRailroads();
+//    public int getRentCost() {
+//        int ownedRailroads = getAmountOwnedRailroads();
+//
+//        if (ownedRailroads == 4) {
+//            return fourRailroadRent;
+//        } else if (ownedRailroads == 3) {
+//            return threeRailroadRent;
+//        } else if (ownedRailroads == 2) {
+//            return twoRailroadRent;
+//        } else {
+//            return oneRailroadRent;
+//        }
+//    }
 
-        if (ownedRailroads == 4) {
-            return fourRailroadRent;
-        } else if (ownedRailroads == 3) {
-            return threeRailroadRent;
-        } else if (ownedRailroads == 2) {
-            return twoRailroadRent;
-        } else {
-            return oneRailroadRent;
-        }
-    }
-
-    private int getAmountOwnedRailroads() {
-        int ownedRailroads = 0;
-
-        for (Field property : owner.getProperties()) {
-            if (property.getFieldType().equals(FieldType.RAILROADS)) {
-                ownedRailroads++;
-            }
-        }
-        return ownedRailroads;
-    }
+//    private int getAmountOwnedRailroads() {
+//        int ownedRailroads = 0;
+//
+//        for (Field property : owner.getProperties()) { //todo findOwnerById
+//            if (property.getFieldType().equals(FieldType.RAILROADS)) {
+//                ownedRailroads++;
+//            }
+//        }
+//        return ownedRailroads;
+//    }
 }
